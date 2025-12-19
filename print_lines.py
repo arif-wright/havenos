58d0b8110d09739598140bf94bc5354c7c8deb4e
arif-wright <arif.wright@gmail.com>
2025-12-19 11:34:34 -0600

    Remove_temp_files

--- print_lines.py
+++
@@ -1,8 +0,0 @@
-import pathlib
-import sys
-path = pathlib.Path(sys.argv[1])
-lines = path.read_text().splitlines()
-start = int(sys.argv[2]) if len(sys.argv)  else 
-end = int(sys.argv[3]) if len(sys.argv)  else len(lines)
-for i in range(start-1, min(end, len(lines))):
-tprint(f\"{i+1}:{lines[i]}\")
--- temp.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs'); 
-const lines = fs.readFileSync('supabase/schema.sql','utf8').split(/\r?\n/); 
-lines.forEach((line,i)=
