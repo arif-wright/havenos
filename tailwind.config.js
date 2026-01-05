86913f52bbcd4a130ae82305fbe8379d984f7a4b
arif-wright <arif.wright@gmail.com>
2026-01-05 12:07:53 -0600

    phase 3.83

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,21 @@ export default {
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif']
+				sans: [
+					'Inter',
+					'Inter Variable',
+					'SF Pro Display',
+					'-apple-system',
+					'BlinkMacSystemFont',
+					'Segoe UI',
+					'Roboto',
+					'Oxygen',
+					'Ubuntu',
+					'Cantarell',
+					'Open Sans',
+					'Helvetica Neue',
+					'sans-serif'
+				]
 			}
 		}
 	},
