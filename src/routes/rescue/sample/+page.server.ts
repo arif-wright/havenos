5df3998eb17707f53caf0e981e59b3b2ae866467
arif-wright <arif.wright@gmail.com>
2026-01-06 17:28:48 -0600

    phase 4.36

--- src/routes/rescue/sample/+page.server.ts
+++ src/routes/rescue/sample/+page.server.ts
@@ -75,7 +75,7 @@ const sampleAnimals = [
 		animal_photos: [
 			{
 				image_url:
-					'https://images.unsplash.com/photo-1507146426996-ef05306b995a?auto=format&fit=crop&w=900&q=80'
+					'https://images.unsplash.com/photo-1543466835-00a7907e9de1?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 			}
 		]
 	},
